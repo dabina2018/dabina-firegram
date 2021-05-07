be4d122ce343b6bf1d67a7e76956f2ab40cb493f
dabina2018 <daveena.cooper18@outlook.com>
2021-05-07 00:04:32 -0400

    added snapshot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
-//import ProgressBar from './comps/ProgressBar';
+import ImageGrid from './comps/ImageGrid';
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadForm />
+      <ImageGrid />
 
     </div>
   );
